d37516a9035f78c3b975efef6308526eecbd6d9e
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-04-05 20:40:19 +0200

    Update AllMethods.R
    Updated all the methods

--- R/AllMethods.R
+++ R/AllMethods.R
@@ -1,75 +1,128 @@
-##################################################################
-## GET data
-##################################################################
+#### Methods ####
+### get ###
+## extracts the latitude from the objects ##
+setGeneric('getLat', function(object, units = 'rad')
+{standardGeneric('getLat')})
 
+## extracts the data for class Meteo ##
 setGeneric('getData', function(object){standardGeneric('getData')})
 
-setMethod('getData',##Solo definido para Meteo, de forma que siempre devuelve valores de partida
-          signature=(object='Meteo'),
-          definition=function(object){
-            result=object@data
-            return(result)
-          }
-          )
-
+## extracts the global irradiance for class Meteo ##
 setGeneric('getG0', function(object){standardGeneric('getG0')})
 
-setMethod('getG0',##Solo definido para Meteo, de forma que siempre devuelve valores de partida
-          signature=(object='Meteo'),
-          definition=function(object){
-            result=getData(object)
-            return(result$G0)
-          }
-          )
+### index ###
+## extract the index of the daily data ##
+setGeneric('indexD', function(object){standardGeneric('indexD')})
 
-###Latitud
-setGeneric('getLat', function(object, units='rad'){standardGeneric('getLat')})
+## extract the index of the intradaily data ##
+setGeneric('indexI', function(object){standardGeneric('indexI')})
 
+
+#### Methods for Sol ####
+### getLat ###
 setMethod('getLat',
-          signature=(object='Sol'),
-          definition=function(object, units='rad'){
-            stopifnot(units %in% c('deg', 'rad'))
-            res=switch(units,
-              rad=d2r(object@lat),
-              deg=object@lat)
-            return(res)
+          signature = (object = 'Sol'),
+          definition = function(object, units = 'rad'){
+              stopifnot(units %in% c('deg', 'rad'))
+              result = switch(units,
+                           rad = d2r(object@lat),
+                           deg = object@lat)
+              return(result)
+          })
+
+### show ###
+setMethod('show',
+          signature = (object = 'Sol'),
+          definition = function(object){
+              cat('Object of class Sol \n\n')
+              cat('Latitude: ',
+                  paste(round(getLat(object, 'deg'), 1), 'degrees\n\n'))
+              cat('Daily values:\n')
+              print(summary(object@solD))
+              cat('\nIntradaily values: \n')
+              print(summary(object@solI))
+          })
+
+### indexD ###
+setMethod('indexD',
+          signature = (object = 'Sol'),
+          definition = function(object){object@solD[, .(Dates)]
+          })
+
+
+### indexI ###
+setMethod('indexI',
+          signature = (object = 'Sol'),
+          definition = function(object){
+              result <- data.table(Dates = object@solI[['Dates']])
+              return(result)
+          })
+
+
+#### Methods for Meteo ####
+### getData ####
+setMethod('getData',
+          signature = (object = 'Meteo'),
+          definition = function(object){
+              result <- object@data
+              return(result)
+          })
+
+### getLat ###
+setMethod('getLat',
+          signature = (object = 'Meteo'),
+          definition = function(object, units = 'rad'){
+              stopifnot(units %in% c('deg', 'rad'))
+              result = switch(units,
+                           rad = d2r(object@lat),
+                           deg = object@lat)
+              return(result)
+          })
+
+### show ###
+setMethod('show', 'Meteo',
+          function(object){
+            cat('Object of class ', class(object),'\n\n')
+            cat('Source of meteorological information: ')
+            cat(paste(object@type, object@source, sep='-'),'\n')
+            cat('Latitude of source: ',
+                paste(round(getLat(object,'deg'), 1), 'degrees\n\n'))
+            cat('Meteorological Data:\n')
+            print(summary(getData(object)))
           }
           )
 
-setMethod('getLat',
-          signature=(object='Meteo'),
-          definition=function(object, units='rad'){
-            stopifnot(units %in% c('deg', 'rad'))
-            res=switch(units,
-              rad=d2r(object@latData),
-              deg=object@latData)
-            return(res)
-          }
-          )
+### indexD ###
+setMethod('indexD',
+          signature = (object = 'Meteo'),
+          definition = function(object){
+              result <- getData(object)$Dates
+              return(result)
+          })
+
+### indexI ###
+setMethod('indexI',
+          signature = (object = 'Meteo'),
+          definition = function(object){
+              result <- getData(object)$Dates
+              return(result)
+          })
+
+#### Methods for G0 ####
+
+### getG0 ###
+setMethod('getG0',
+          signature = (object = 'Meteo'),
+          definition = function(object){
+              result <- getData(object)
+              return(result$G0)
+          })
+
+### getLat ###
 setMethod('getLat',
           signature=(object='G0'),
           definition=function(object, units='rad'){
             getLat(as(object, 'Sol'), units=units)
-          }
-          )
-
-##################################################################
-## INDEX
-##################################################################
-
-
-setGeneric('indexD', function(object){standardGeneric('indexD')})
-setMethod('indexD',
-          signature=(object='Meteo'),
-          definition=function(object){
-            return(index(object@data))
-          }
-          )
-
-setMethod('indexD',
-          signature=(object='Sol'),
-          definition=function(object){
-            return(index(object@solD))
           }
           )
 
@@ -79,24 +132,3 @@ setMethod('indexD',
             indexD(as(object, 'Sol'))
           }
           )
-
-
-setGeneric('indexI', function(object){standardGeneric('indexI')})
-setMethod('indexI',
-          signature=(object='Sol'),
-          definition=function(object){
-            return(index(object@solI))
-          }
-          )
-
-setGeneric('indexRep', function(object){standardGeneric('indexRep')})
-setMethod('indexRep',
-          signature=(object='Sol'),
-          definition=function(object){
-            return(object@match)
-          }
-          )
-
-
-
-
