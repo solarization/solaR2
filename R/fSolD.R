ce261331ac971a0ab1f1d0dea4f92b7da3445804
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-04-05 21:10:36 +0200

    Update fSolD.R

--- R/fSolD.R
+++ R/fSolD.R
@@ -1,27 +1,33 @@
-fSolD <- function(d, lat, method="michalsky"){
-  if (abs(lat) > 90){
-    lat <- sign(lat) * 90
-    warning(paste("Latitude outside acceptable values. Set to", lat))
-  }
-  d <- as.IDate(d)
-  ##Declination
-  decl <- declination(d, method = method)
-  ##Distancia sol-tierra, 1/r2
-  ##ro=1.496E8                        #distancia media Tierra-Sol (km)
-  eo <- eccentricity(d, method = method)
-  EoT <- eot(d)
-  ws <- sunrise(d, lat, method = method,
-                  decl = decl)
-  Bo0d <- bo0d(d, lat, method = method,
-               ws = ws, decl = decl, eo = eo)
+fSolD <- function(d, lat, method = 'michalsky'){
+    if (abs(lat) > 90){
+        lat <- sign(lat) * 90
+        warning(paste('Latitude outside acceptable values. Set to', lat))
+    }
+    Dates <- unique(as.IDate(d))
 
-  result <- data.table(date = d,
-                       decl = decl,
-                       eo = eo,
-                       ws = ws,
-                       Bo0d = Bo0d,
-                       EoT = EoT)
-  attr(result, "lat") = lat
-  setkey(result, date)
-  result
+    #### solarAngles ####
+    
+    ##Declination
+    decl <- declination(Dates, method = method)
+    ##Eccentricity
+    eo <- eccentricity(Dates, method = method)
+    ##Equation of time
+    EoT <- eot(Dates)
+    ##Solar time
+    ws <- sunrise(Dates, lat, method = method,
+                  decl = decl)
+    ##Extraterrestrial irradiance
+    Bo0d <- bo0d(Dates, lat, method = method,
+                 ws = ws, decl = decl, eo = eo)
+
+    result <- data.table(Dates = Dates,
+                         lat = lat,
+                         decl = decl,
+                         eo = eo,
+                         EoT = EoT,
+                         ws = ws,
+                         Bo0d = Bo0d)
+    attr(result, 'lat') = lat
+    setkey(result, Dates)
+    result
 }
