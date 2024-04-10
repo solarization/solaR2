65c2e7a73ad120cf74daa4e81506ee1e6f4aa483
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-04-10 16:43:16 +0200

    Update solarAngles.R

--- R/solarAngles.R
+++ R/solarAngles.R
@@ -1,57 +1,71 @@
-## Declination
-declination <- function(x, method = 'michalsky')
+#### Declination ####
+declination <- function(d, method = 'michalsky')
 {
-    ## x is a IDate
-    x <- as.IDate(x)
-    ## Day of Year
-    dn <- yday(x)
+    ##Method comprobation
+    if(!(method %in% c("michalsky", "cooper", "strous", "spencer"))){
+        warning("'method' must be: michalsky, cooper, strous or spencer. Set michalsky")
+        method = 'michalsky'
+    }
+
+    ## x is an IDate
+    d <- as.IDate(d)
+    ## Day of year
+    dn <- yday(d)
     ## Days from 2000-01-01
     origin <- as.IDate('2000-01-01')
-    jd <- as.numeric(x - origin)
-    X <- 2 * pi * (dn-1)/365
-    
+    jd <- as.numeric(d - origin)
+    X <- 2 * pi * (dn - 1) / 365
+
     switch(method,
-           cooper = {
-               ##Cooper, P.I., Solar Energy, 12, 3 (1969). "The Absorption of Solar Radiation in Solar Stills"
-               d2r(23.45) * sin(2 * pi * (dn + 284)/365)
+           michalsky = {
+           meanLong <- (280.460 + 0.9856474 * jd)%%360
+           meanAnomaly <- (357.528 + 0.9856003 * jd)%%360
+           eclipLong <- (meanLong +1.915 * sin(d2r(meanAnomaly)) +
+                         0.02 * sin(d2r(2 * meanAnomaly)))%%360
+           excen <- 23.439 - 0.0000004 * jd
+           sinEclip <- sin(d2r(eclipLong))
+           sinExcen <- sin(d2r(excen))
+           asin(sinEclip * sinExcen)
            },
-           spencer = {
-               ##Spencer, Search 2 (5), 172
-               ##http://www.mail-archive.com/sundial@uni-koeln.de/msg01050.html
-               0.006918 - 0.399912 * cos(X) + 0.070257 * sin(X) -
-                   0.006758 * cos(2 * X) + 0.000907 * sin(2 * X) -
-                       0.002697 * cos(3 * X) + 0.001480 * sin(3 * X) 
-           }, 
-           strous={
-               meanAnomaly  <-  (357.5291 + 0.98560028 * jd)%%360
+           cooper = {
+               ##P.I. Cooper. “The Absorption of Solar Radiation in Solar Stills”. Solar Energy 12 (1969).
+               d2r(23.45) * sin(2 * pi * (dn +284) / 365)
+           },
+           strous = {
+               meanAnomaly <- (357.5291 + 0.98560028 * jd)%%360
                coefC <- c(1.9148, 0.02, 0.0003)
                sinC <- sin(outer(1:3, d2r(meanAnomaly), '*'))
-               C  <-  colSums(coefC * sinC)
+               C <- colSums(coefC * sinC)
                trueAnomaly <- (meanAnomaly + C)%%360
                eclipLong <- (trueAnomaly + 282.9372)%%360
                excen <- 23.435
                sinEclip <- sin(d2r(eclipLong))
                sinExcen <- sin(d2r(excen))
                asin(sinEclip * sinExcen)
-           }, 
-           michalsky={
-               meanLong <- (280.460+0.9856474 * jd)%%360
-               meanAnomaly <- (357.528+0.9856003 * jd)%%360
-               eclipLong <- (meanLong +1.915 * sin(d2r(meanAnomaly))+0.02 * sin(d2r(2 * meanAnomaly)))%%360
-               excen <- 23.439-0.0000004 * jd
-               sinEclip <- sin(d2r(eclipLong))
-               sinExcen <- sin(d2r(excen))
-               asin(sinEclip * sinExcen)
            },
-           stop('Unknown method')
-           )
+           spencer = {
+               ## J.W. Spencer. “Fourier Series Representation of the Position of the Sun”. 2 (1971).
+               ##URL: http://www.mail-archive.com/sundial@uni-koeln.de/msg01050.html.
+               0.006918 - 0.399912 * cos(X) + 0.070257 * sin(X) -
+                   0.006758 * cos(2 * X) + 0.000907 * sin(2 * X) -
+                       0.002697 * cos(3 * X) + 0.001480 * sin(3 * X)           
+           })
 }
-
-eccentricity <- function(x, method = 'michalsky')
+
+
+#### Eccentricity ####
+eccentricity <- function(d, method = 'michalsky')
 {
-    x <- as.IDate(x)
-    ## Day of Year
-    dn <- yday(x)
+        ##Method comprobation
+    if(!(method %in% c("michalsky", "cooper", "strous", "spencer"))){
+        warning("'method' must be: michalsky, cooper, strous or spencer. Set michalsky")
+        method = 'michalsky'
+    }
+    
+    ##x is an IDate
+    d <- as.IDate(d)
+    ##Day of year
+    dn <- yday(d)
     X <- 2 * pi * (dn-1)/365
 
     switch(method,
@@ -60,125 +74,101 @@ eccentricity <- function(x, method = 'michalsky')
            michalsky = , 
            strous = 1.000110 + 0.034221*cos(X) +
                0.001280*sin(X) + 0.000719*cos(2*X) +
-                   0.000077*sin(2*X),
-           stop('Unknown method')
+               0.000077*sin(2*X)
            )
 }
-
-sunrise <- function(x, lat, ...,
-                      decl = declination(x, ...))
+
+
+#### Equation of time
+
+##Alan M.Whitman "A simple expression for the equation of time"
+##EoT=ts-t, donde ts es la hora solar real y t es la hora solar
+##media. Valores negativos implican que el sol real se retrasa
+##respecto al medio
+eot <- function(d)
+{
+    ## d in an IDate
+    d <- as.IDate(d)
+    ## Day of year
+    dn <- yday(d)
+    M <- 2 * pi/365.24 * dn
+    EoT.min <- 229.18 * (-0.0334 * sin(M) +
+                         0.04184 * sin(2 * M + 3.5884))
+}
+
+
+#### Solar time ####
+sunrise <- function(d, lat, ...,
+                    decl = declination(d, ...))
 {
     cosWs <- -tan(d2r(lat)) * tan(decl)
     #sunrise, negative since it is before noon
     ws <- suppressWarnings(-acos(cosWs))
-    ##Polar day/night
-    polar <- which(is.nan(ws))        
+    #Polar day/night
+    polar <- which(is.nan(ws))
     ws[polar] <- -pi * (cosWs[polar] < -1) + 0 * (cosWs[polar] > 1)
-    ws    
+    ws
 }
 
-dayLength <- function(x, lat, ...)
+#### Extraterrestrial irradition ####
+bo0d <- function(d, lat, ...,
+                 decl = declination(d, ...),
+                 eo = eccentricity(d, ...),
+                 ws = sunrise(d, lat, ...))
 {
-    ws <- sunrise(x, lat, ...)
-    2 * abs(ws)
-}
-
-##Equation of Time, minutes según Alan M.Whitman "A simple expression
-##for the equation of time" EoT=ts-t, donde ts es la hora solar real y
-##t es la hora solar media. Valores negativos implican que el sol real
-##se retrasa respecto al medio
-eot <- function(x){
-    ## x is a IDate
-    x <- as.IDate(x)
-    ## Day of Year
-    dn <- yday(x)
-    M <- 2 * pi/365.24 * dn
-    EoT.min <- 229.18 * (-0.0334 * sin(M) +
-                             0.04184 * sin(2 * M + 3.5884))
-    ## Radians
-    h2r(EoT.min / 60)                   
-}
-
-## Extraterrestrial Irradiance
-bo0d <- function(x, lat, ...,
-                 ws = sunrise(x, lat, ...),
-                 decl = declination(x, ...),
-                 eo = eccentricity(x, ...)
-                 )
-{
-    ## solar constant
+    #solar constant
     Bo <- 1367
     lat <- d2r(lat)
-    ## El signo negativo se debe a la definición de ws
+    #The negative sign due to the definition of ws
     -24/pi * Bo * eo * (ws * sin(lat) * sin(decl) +
-                            cos(lat) * cos(decl) * sin(ws)
-                        ) 
+                        cos(lat) * cos(decl) * sin(ws))
 }
-
-## Solar Hour Angle
-sunHour <- function(x, method = 'michalsky', EoT = 0)
+
+
+#### Sun hour angle ####
+sunHour <- function(lon = 0, BTi, GMT = 0)
 {
-    if (inherits(x, 'data.table')) {
-            tt <- x[, as.POSIXct(date, time)]
-            time <- x$time
+    if (inherits(BTi, 'data.table')) {
+            tt <- BTi[, as.POSIXct(Dates, Times, tz = 'CET')]
+            Times <- BTi$Times
+            Dates <- BTi$Dates
         }
     else {
-        tt <- as.POSIXct(x, tz = 'UTC')
-        time <- as.ITime(tt)
+        tt <- as.POSIXct(BTi, tz = 'CET')
+        Times <- as.ITime(tt)
+        Dates <- as.IDate(tt)
+        }
         
-    }
-    ## Official Local time
-    TO <- as.numeric(time)/3600 ## hours
-    ## Days from 2000-01-01
-    origin <- as.POSIXct('2000-01-01 12:00:00 UTC', tz = 'UTC')
-    jd <- as.numeric(tt - origin)
+    TO <- as.numeric(Times)/3600
+    AO <- AO(tt)
+    lambda <- lon - GMT * 15
+    EoT <- eot(Dates)
+    w <- 15 * (TO - AO - 12) + lambda + EoT/4
+    d2r(w)
+}
 
-    switch(method,
-           cooper = ,
-           spencer = {
-               h2r(TO - 12) + EoT
-           },
-           michalsky = {
-               meanLong <- (280.460 + 0.9856474 * jd) %% 360
-               meanAnomaly <- (357.528 + 0.9856003 * jd) %% 360
-               eclipLong <- (meanLong + 1.915 * sin(d2r(meanAnomaly)) +
-                                 0.02 * sin(d2r(2*meanAnomaly))) %% 360
-               excen <- 23.439 - 0.0000004 * jd
-               
-               sinEclip <- sin(d2r(eclipLong))
-               cosEclip <- cos(d2r(eclipLong))
-               cosExcen <- cos(d2r(excen))
-               
-               ascension <- r2d(atan2(sinEclip * cosExcen,
-                                      cosEclip)) %% 360
+#### Daylight saving time ####
+AO <- function(tt)
+{
+    as.POSIXlt(tt)$isdst
+}
 
-               ##local mean sidereal time, LMST. TO has been previously
-               ##corrected with local2Solar in order to include the
-               ##longitude, daylight savings, etc.
-               lmst <- (h2d(6.697375 + 0.0657098242 * jd + TO)) %% 360
-               w <- (lmst - ascension)
-               d2r(w + 360 * (w < -180) - 360 * (w > 180))
-           },
-           strous = {
-               meanAnomaly  <-  (357.5291 + 0.98560028*jd)%%360
-               coefC <- c(1.9148, 0.02, 0.0003)
-               sinC <- sin(outer(1:3, d2r(meanAnomaly), '*'))
-               C  <-  colSums(coefC * sinC)
-               trueAnomaly <- (meanAnomaly + C) %% 360
-               eclipLong <- (trueAnomaly + 282.9372) %% 360
-               excen <- 23.435
-               
-               sinEclip <- sin(d2r(eclipLong))
-               cosEclip <- cos(d2r(eclipLong))
-               cosExcen <- cos(d2r(excen))
-               
-               ascension <- r2d(atan2(sinEclip * cosExcen,
-                                      cosEclip)) %% 360
-               
-               ##local mean sidereal time, LMST
-               lmst <- (280.1600 + 360.9856235 * jd)%%360
-               w <- (lmst - ascension)
-               d2r(w + 360 * (w < -180) - 360 * (w > 180))
-           }
-           )
+#### zenith angle ####
+zenith <- function(decl, lat, w)
+{
+    zenith <- sin(decl) * sin(lat) +
+        cos(decl) * cos(w) * cos(lat)
+    zenith <- ifelse(zenith > 1, 1, zenith)
+    zenith
+}
+
+#### azimuth ####
+azimuth <- function(solD,decl, w, lat, AlS)
+{
+    lat <- d2r(attr(solD, 'lat'))
+    signLat <- ifelse(sign(lat) == 0, 1, sign(lat)) #if the sign of lat is 0, it changes it to 1
+    azimuth <- signLat * (cos(decl) * cos(w) * sin(lat) -
+                          cos(lat) * sin(decl)) / cos(AlS)
+    azimuth <- ifelse(abs(azimuth) > 1, 1 * sign(azimuth), azimuth)
+    azimuth
 }
