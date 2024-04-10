f074f57419e2666af4d8204965f92eac296a1c47
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-04-10 16:43:19 +0200

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -1,134 +1,77 @@
-###Indices temporales
+#### Angles ####
+#degrees to radians
+d2r<-function(x){x*pi/180}
 
-###Quizás sería útil que todas estas funciones admitiesen un zoo directamente
+#radians to degrees
+r2d<-function(x){x*180/pi}
+
+#hours to radians
+h2r<-function(x){x*pi/12}
+
+#hours to degrees
+h2d<-function(x){x*180/12}
+
+#radians to hours
+r2h<-function(x){x*12/pi}
+
+#degrees to hours
+d2h<-function(x){x*12/180}
+
+#radians to seconds
+r2sec<-function(x){x*12/pi*3600}
+
+#radians to minutes
+r2min<-function(x){x*12/pi*60}
+
+#### Time ###
+#hour
 hour <- function(x) 
 {
     as.numeric(format(x, "%H"))
 }
 
+#minute
 minute <- function(x)
 {
     as.numeric(format(x, "%M"))
 }
 
+#second
 second <- function(x) 
 {
     as.numeric(format(x, "%S"))
 }
 
-hms <- function(x)
+#complete time to hours
+t2h <- function(x)
 {
     hour(x)+minute(x)/60+second(x)/3600
 }
 
+#day of the year
 doy <- function(x){
   as.numeric(format(x, '%j'))
 }
 
+#day of the month
 dom <- function(x){
   as.numeric(format(x, '%d'))
 }
 
+#month
 month <- function(x){
   as.numeric(format(x, '%m'))
 }
 
+#year
 year <- function(x){
   as.numeric(format(x, '%Y'))
-
 }
 
-DoY <- function(x){format(x, '%j')}
+#trunc days
+truncDay <- function(x){as.POSIXct(trunc(x, units='days'))}
 
-DoM <- function(x){format(x, '%d')}
-
-Month <- function(x){format(x, '%m')}
-
-Year <- function(x){format(x, '%Y')}
-
-dst <- function(x)                      #Adelanto horario por verano
-   {
-     as.POSIXlt(x)$isdst
-   }
-
-##Angulos
-
-d2r <- function(x){x*pi/180}
-
-r2d <- function(x){x*180/pi}
-
-h2r <- function(x){x*pi/12}
-h2d <- function(x){x*180/12}
-
-r2h <- function(x){x*12/pi}
-d2h <- function(x){x*12/180}
-
-r2sec <- function(x){x*12/pi*3600}
-
-
-##Trunca un POSIXct a días
-truncDay  <-  function(x){as.POSIXct(trunc(x, units='days'))}
-
-## Check if daily indexes are equal (used in fCompD and fTemp)
-checkIndexD <- function(ix, iy)
-{
-    dx <- truncDay(ix)
-    dy <- truncDay(iy)
-    test <- all.equal(dx, dy,  check.attributes = FALSE)
-    if (!isTRUE(test))
-        stop('daily indexes do not match.')
-}
-
-
-###Husos horarios
-lonHH<-function(tz)
-    {            #Calcula la longitud (en radianes) de un huso horario
-      stopifnot(class(tz)=='character')
-      tHH <- as.POSIXct('2000-1-1 12:00:00', tz=tz)
-      tUTC <- as.POSIXct(format(tHH, tz='UTC'), tz=tz)
-      h2r(as.numeric(tHH-tUTC))
-    }
-
-  
-local2Solar <- function(x, lon=NULL){	
-  tz=attr(x, 'tzone')
-  if (tz=='' || is.null(tz)) {tz='UTC'}
-  ##Adelanto oficial por verano
-  AO=3600*dst(x)
-  AOneg=(AO<0)
-  if (any(AOneg)) {
-    AO[AOneg]=0
-    warning('Some Daylight Savings Time unknown. Set to zero.')
-  }
-  ##Diferencia entre la longitud del lugar y la longitud del huso horario LH
-  LH=lonHH(tz)
-  if (is.null(lon)) 
-    {deltaL=0
-   } else
-  {deltaL=d2r(lon)-LH
- }
-  ##Hora local corregida en UTC
-  ##    tt <- format(x-AO+r2sec(deltaL), tz=tz)
-  tt <- format(x, tz=tz)
-  result <- as.POSIXct(tt, tz='UTC')-AO+r2sec(deltaL)
-  ##      result <- as.POSIXct(tt, tz='UTC')
-  result
-}
-
-
-##cbind garantizando conservación del index (para tz='UTC', principalmente)
-CBIND <- function(..., index=NULL){
-  args <- list(...)
-  cdata <- lapply(args, coredata)
-  result0 <- as.data.frame(cdata)
-  if (is.null(index)){
-    return(zoo(result0, index(args[[1]])))
-  } else {
-    return(zoo(result0, index))
-  }
-}
-
-##Convierte un difftime en un número de horas
+##difftime to hours
 diff2Hours  <- function(by){
   if (!inherits(by, 'difftime')) {
     stop('This function is only useful for difftime objects.')
@@ -137,6 +80,7 @@ diff2Hours  <- function(by){
   }
 }
 
+# character to difftime
 char2diff <- function(by){
   if (!is.character(by)) {
     stop('This function is only useful for character strings.')
@@ -162,6 +106,7 @@ char2diff <- function(by){
   }
 }
 
+# sample to hours
 sample2Hours <- function(by){
   if (is.character(by)) {
     y <- char2diff(by)
@@ -171,73 +116,10 @@ sample2Hours <- function(by){
   } else {stop('by must be a character or difftime.')}
 }
   
+
+#### Energy ####
+# Power to energy
 P2E <- function(x, by){
-  Nm=1/sample2Hours(by)
-  sum(x, na.rm=1)/Nm                    #Potencia a Energía
-} 
-###OJO: no exportadas
-solvePac <- function(x, Cinv){
-  Vdc=x[1]
-  PdcN=x[2]
-  V <- c(1, Vdc, Vdc^2)
-  Ki=t(colSums(V*t(Cinv)))
-  A=Ki[3]
-  B=Ki[2]+1
-  C=Ki[1]-(PdcN)
-  result <- (-B+sqrt(B^2-4*A*C))/(2*A)
-  result
+    Nm <- 1/sample2Hours(by)
+    sum(x, na.rm = 1)/Nm
 }
-
-dailySum <- function(x, by){##x is a time series
-  if (missing(by)) {by=DeltaT(x)}
-  res <- aggregate(x, by=truncDay, FUN=P2E, by)
-  return(res)
-  }
-
-monthlySum <- function(x, by){##x is a INTRADAILY time series
-  if (missing(by)) {by=DeltaT(x)}
-  res <- aggregate(x, by=as.yearmon, FUN=P2E, by)
-  return(res)
-  }
-
-yearlySum <- function(x, by){##x is a INTRADAILY time series
-  if (missing(by)) {by=DeltaT(x)}
-  res <- aggregate(x, by=year, FUN=P2E, by)
-  return(res)
-  }
-
-dailyMean <- function(x){##x is a time series
-  res <- aggregate(x, by=doy, FUN=mean, na.rm=1)
-  return(res)
-  }
-
-monthlyMean <- function(x){##x is a time series
-  res <- aggregate(x, by=as.yearmon, FUN=mean, na.rm=1)
-  return(res)
-  }
-
-yearlyMean <- function(x){##x is a time series
-  res <- aggregate(x, by=year, FUN=mean, na.rm=1)
-  return(res)
-  }
-
-
-##No exportada
-DeltaT <- function(x){
-  return(median(diff(index(x))))###spend a long time with large series, ¿mean(x, 0.2)?
-  }
-
-
-factorI<-function(x, index.rep, breaks=3, ...){
-  ##x es una variable extraida con $ de un slot de un objeto
-  ##index.rep es el índice que relaciona las variables diarias con las instantátneas.
-  ##Se obtiene con indexRep(object)
-  var.fac<-cut(x, breaks=breaks, ...)
-  ## indexI.day<-as.POSIXct(trunc(indexI, 'day'))
-  ## mtch<-match(indexI.day, indexD, nomatch=0)
-  result<-var.fac[index.rep]
-}
-
-
-
-  
