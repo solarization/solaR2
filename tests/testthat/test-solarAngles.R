ccae2861014f1895584e2a18becd61e1618dd9f3
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-04-13 10:51:10 +0200

    Update test-solarAngles.R
    Added the test for eccentricity

--- tests/testthat/test-solarAngles.R
+++ tests/testthat/test-solarAngles.R
@@ -4,3 +4,10 @@ test_that("declination in certain days", {
     decl_expected <- c(-0.36271754, -0.22850166, -0.03191616, 0.17531794, 0.33246485, 0.40257826, 0.36439367, 0.22407398, 0.02730595, -0.17900474, -0.33862399, -0.40478283)
     expect_equal(declination(days), decl_expected)
 })
+
+test_that("eccentricity in certain days", {
+    promDays <- c(17, 14, 15, 15, 15, 10, 18, 18, 18, 19, 18, 13)
+    days <- paste("2024", 1:12, promDays, sep = "-")
+    eccen_expected <- c(-20.91696257,-13.61976641,-2.818878653, 9.414893347, 18.79191752, 23.01163673, 21.00743137, 12.78588828, 1.008871364, -11.04869045, -19.82109039,-23.2416248)
+    expect_equal(eccentricity(days), eccen_expected)
+})
