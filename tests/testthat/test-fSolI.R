6da25913c7a1ba9a107e148302a9064017a5df42
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-05-14 20:51:39 +0200

    Create test-fSolI.R

---
+++ tests/testthat/test-fSolI.R
@@ -0,0 +1,25 @@
+test_that('Dates in fSolI',{
+    d <- fBTd(year = 2023)
+    lat <- 37.2
+    solD <- fSolD(d, lat)
+    days_expected <- fBTi(fBTd(year = 2023),
+                          '1 hour')
+    expect_equal(fSolI(solD)$Dates, days_expected)
+})
+
+test_that('Solar time in fSolI',{
+    d <- fBTd(year = 2023)
+    BTi <- fBTi(d, '1 hour')
+    lat <- 37.2
+    solD <- fSolD(d, lat)
+    expect_equal(fSolI(solD, BTi = BTi)$w, sunHour(d, BTi))
+})
+
+#test_that('Zenith angle in fSolI',{
+ #   d <- fBTd(year = 2023)
+ #   BTi <- fBTi(d, '1 hour')
+ #   lat <- 37.2
+ #   solD <- fSolD(d, lat)
+ #   expect_equal(fSolI(solD, BTi = BTi)$cosThzS, zenith(d, lat, BTi))
+#})
+
