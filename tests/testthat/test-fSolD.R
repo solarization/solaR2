04114cca5276dcd5904295a6dc357cce0e9b591f
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-05-14 18:53:09 +0200

    Update test-fSolD.R

--- tests/testthat/test-fSolD.R
+++ tests/testthat/test-fSolD.R
@@ -1,18 +1,18 @@
 test_that('Dates in fSolD',{
-    d <- fBTd()
+    d <- fBTd(year = 2023)
     lat <- 37.2
     days_expected <- as.IDate(d)
     expect_equal(fSolD(d, lat)$Dates, days_expected)
 })
 
 test_that('declination in fSolD',{
-    d <- fBTd()
+    d <- fBTd(year = 2023)
     lat <- 37.2
     expect_equal(fSolD(d, lat)$decl, declination(d))
 })
 
 test_that('eccentricity in fSolD',{
-    d <- fBTd()
+    d <- fBTd(year = 2023)
     lat <- 37.2
     expect_equal(fSolD(d, lat)$eo, eccentricity(d))
 })
@@ -24,13 +24,13 @@ test_that('equation of time in fSolD',{
 })
 
 test_that('solar time in fSolD',{
-    d <- fBTd()
+    d <- fBTd(year = 2023)
     lat <- 37.2
     expect_equal(fSolD(d, lat)$ws, sunrise(d, lat))
 })
 
 test_that('extraterrestrial irradiance in fSolD',{
-    d <- fBTd()
+    d <- fBTd(year = 2023)
     lat <- 37.2
     expect_equal(fSolD(d, lat)$Bo0d, bo0d(d, lat))
 })
