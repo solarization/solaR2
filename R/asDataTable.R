db9ff23b7b581eb252daecdf6be2030aba563bf0
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-07-03 12:32:42 +0200

    Update asDataTable.R

--- R/asDataTable.R
+++ R/asDataTable.R
@@ -1,5 +1,6 @@
 setGeneric('as.data.table')
 
+
 ###as.data.frameI
 setGeneric('as.data.tableI',
            function(object, complete=FALSE, day=FALSE){standardGeneric('as.data.tableI')})
@@ -7,15 +8,14 @@ setGeneric('as.data.tableI',
 setMethod('as.data.tableI',
           signature=(object='Sol'),
           definition=function(object, complete=FALSE, day=FALSE){
-              sun <- object@solI
-              if(day){
-                  sun[, day := doy(Dates)]
-              }
-              if(complete){
-                  sun2 <- as.data.tableD(object)
-                  sun <- data.table(sun, sun2[, -'Dates'])
-              }
-              return(sun)
+              zoo0=as.zooI(object, complete=complete, day=day)
+              data0=as.data.table(zoo0)
+              ind=index(zoo0)
+              ##Incorporo dia, mes y año como columnas del data.table
+              data0[, day := doy(ind)]
+              data0[, month := month(ind)]
+              data0[, year := year(ind)]
+              return(data0)
           }
           )
 
@@ -25,33 +25,44 @@ setGeneric('as.data.tableD', function(object, day = FALSE){standardGeneric('as.d
 setMethod('as.data.tableD',
           signature=(object='Sol'),
           definition=function(object, day = FALSE){
-              sun <- object@solD
-              if(day){
-                  d <- indexD(object)
-                  sun$day <- doy(d)
-                  sun$month <- month(d)
-                  sun$year <- year(d)
-              }
-              return(sun)
+              zoo0=as.zooD(object, complete=complete)
+              data0=as.data.table(zoo0)
+              ind=index(zoo0)
+              ##Incorporo dia, mes y año como columnas del data.table
+              data0[, day := doy(ind)]
+              data0[, month := month(ind)]
+              data0[, year := year(ind)]
+              return(data0)
           }
           )
 
 ###as.data.frameM
-setGeneric('as.data.tableM', function(object){standardGeneric('as.data.tableM')})
+setGeneric('as.data.tableM', function(object, complete = FALSE){standardGeneric('as.data.tableM')})
 
 setMethod('as.data.tableM',
           signature=(object='G0'),
-          definition=function(object){
-              return(object@G0dm)
+          definition=function(object, complete = FALSE){
+              zoo0=as.zooM(object, complete = complete)
+              data0=as.data.table(zoo0)
+              ind=index(zoo0)
+              ##Incorporo mes y año como columnas del data.table
+              data0[, month := month(ind)]
+              data0[, year := year(ind)]
+              return(data0)
           }
           )
 
 ###as.data.frameY
-setGeneric('as.data.tableY', function(object){standardGeneric('as.data.tableY')})
+setGeneric('as.data.tableY', function(object, complete = FALSE){standardGeneric('as.data.tableY')})
 
 setMethod('as.data.tableY',
           signature=(object='G0'),
-          definition=function(object){
-              return(object@G0y)
+          definition=function(object, complete = FALSE){
+              zoo0=as.zooY(object, complete = complete)
+              data0=as.data.table(zoo0)
+              ind=index(zoo0)
+              ##Incorporo año como columna del data.table
+              data0[, year := ind]
+              return(data0)
           }
           )
