00a05ea5211b355855eaed65e5fd0e9859513099
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-07-04 13:26:39 +0200

    Update asDataTable.R

--- R/asDataTable.R
+++ R/asDataTable.R
@@ -12,6 +12,30 @@ setMethod('as.data.tableI',
                   data <- data.table(solI, solD[, Dates := NULL])
               } else{data <- solI}
               if(day){
+                  ind <- indexI(sol)
+                  data[, day := doy(ind)]
+                  data[, month := month(ind)]
+                  data[, year := year(ind)]
+                  )
+              }
+              return(data)
+          }
+          )
+
+setMethod('as.data.tableI',
+          signature = (object='G0'),
+          definition = function(object, complete=FALSE, day=FALSE){
+              g0 <- copy(object)
+              G0I <- g0@G0I
+              solI <- g0@solI
+              if(complete){
+                  data <- data.table(solI, G0I[, Dates := NULL]) 
+              } else{    
+                  G0I[, Kt := NULL]
+                  G0I[, Fd := NULL]
+                  data <- G0I
+              }
+              if(day){
                   ind <- indexI(object)
                   data[, day := doy(ind)]
                   data[, month := month(ind)]
@@ -22,28 +46,69 @@ setMethod('as.data.tableI',
           )
 
 setMethod('as.data.tableI',
-          signature = (object='G0'),
-          definition = function(object, complete=FALSE, day=FALSE){
-              
+          signature = (object='Gef'),
+          definition = function(object, complete=FALSE, Day=FALSE){
+
           }
           )
 
-###as.data.frameD
-setGeneric('as.data.tableD', function(object, day = FALSE){standardGeneric('as.data.tableD')})
+setMethod('as.data.tableI',
+          signature = (object='ProdGCPV'),
+          definition = function(object, complete=FALSE, Day=FALSE){
+
+          }
+          )
+
+setMethod('as.data.tableI',
+          signature = (object='ProdPVPS'),
+          definition = function(object, complete=FALSE, Day=FALSE){
+
+          }
+          )
+
+###as.data.tableD
+setGeneric('as.data.tableD', function(object, complete=FALSE, day=FALSE){standardGeneric('as.data.tableD')})
 
 setMethod('as.data.tableD',
           signature=(object='Sol'),
-          definition=function(object, day = FALSE){
-              zoo0=as.zooD(object, complete=complete)
-              data0=as.data.table(zoo0)
-              ind=index(zoo0)
-              ##Incorporo dia, mes y año como columnas del data.table
-              data0[, day := doy(ind)]
-              data0[, month := month(ind)]
-              data0[, year := year(ind)]
-              return(data0)
+          definition=function(object, complete=FALSE, day=FALSE){
+              sol <- copy(object)
+              solD <- sol@solD
+              data <- solD
+              if(day){
+                  ind <- indexD(object)
+                  data[, day := doy(ind)]
+                  data[, month := month(ind)]
+                  data[, year := year(ind)]
+              }
+              return(data)
           }
           )
+
+setMethod('as.data.tableD',
+          signature = (object='G0'),
+          definition = function(object, complete=FALSE, day=FALSE){
+              g0 <- copy(object)
+              G0D <- g0@G0D
+              solD <- g0@solD
+              if(complete){
+                  data <- data.table(G0D, solD[, Dates := NULL])
+              } else {data <- G0d}
+              if(days){
+                  ind <- indexD(object)
+                  data[, day := doy(ind)]
+                  data[, month := month(ind)]
+                  data[, year := year(ind)]
+              }
+              return(data)
+          })
+
+setMethod('as.data.tableD',
+          signature = (object='Gef'),
+          definition = function(object, complete=FALSE, day=FALSE){
+              
+          })
+
 
 ###as.data.frameM
 setGeneric('as.data.tableM', function(object, complete = FALSE){standardGeneric('as.data.tableM')})
