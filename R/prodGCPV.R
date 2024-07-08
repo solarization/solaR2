d3e2fbb35da04c09f5424bd217708f8e41d3cee7
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-07-08 11:38:06 +0200

    Update prodGCPV.R

--- R/prodGCPV.R
+++ R/prodGCPV.R
@@ -14,7 +14,7 @@ prodGCPV<-function(lat,
                    effSys=list(), 
                    modeShd='',    
                    struct=list(), 
-                   distances=data.frame(),
+                   distances=data.table(),
                    ...){
 
   stopifnot(is.list(module),
@@ -22,7 +22,7 @@ prodGCPV<-function(lat,
             is.list(inverter),
             is.list(effSys),
             is.list(struct),
-            is.data.frame(distances))
+            is.data.table(distances))
 	
   if (('bt' %in% modeShd) & (modeTrk!='horiz')) {
     modeShd[which(modeShd=='bt')]='area'
@@ -69,31 +69,66 @@ prodGCPV<-function(lat,
   DayOfMonth=c(31,28,31,30,31,30,31,31,30,31,30,31) ###OJO
   Pg=generator$Pg                                   #Wp
   
-  if (radEf@type=='prom') {
-    prodDm=aggregate(prodI[,c('Pac', 'Pdc')]/1000,
-      by=as.yearmon, FUN=P2E, radEf@sample) #kWh
-    names(prodDm)=c('Eac', 'Edc')
-    prodDm$Yf=prodDm$Eac/(Pg/1000)
+  #if (radEf@type=='prom') {
+    #prodDm=aggregate(prodI[,c('Pac', 'Pdc')]/1000,
+      #by=as.yearmon, FUN=P2E, radEf@sample) #kWh
+    #names(prodDm)=c('Eac', 'Edc')
+    #prodDm$Yf=prodDm$Eac/(Pg/1000)
     
-    prodD=prodDm*1000                   #Wh
-    prodD$Yf=prodD$Yf/1000
-    index(prodD) <- indexD(radEf) ##para que sea compatible con G0D
+    #prodD=prodDm*1000                   #Wh
+    #prodD$Yf=prodD$Yf/1000
+    #index(prodD) <- indexD(radEf) ##para que sea compatible con G0D
 
-    prody=zoo(t(colSums(prodDm*DayOfMonth)),
-      unique(year(index(prodDm))))
-  } else {
-    prodD=aggregate(prodI[,c('Pac', 'Pdc')],
-      by=truncDay, FUN=P2E, radEf@sample) #Wh
-    names(prodD)=c('Eac', 'Edc')
-    prodD$Yf=prodD$Eac/Pg
+    #prody=zoo(t(colSums(prodDm*DayOfMonth)),
+      #unique(year(index(prodDm))))
+  #} else {
+    #prodD=aggregate(prodI[,c('Pac', 'Pdc')],
+      #by=truncDay, FUN=P2E, radEf@sample) #Wh
+    #names(prodD)=c('Eac', 'Edc')
+    #prodD$Yf=prodD$Eac/Pg
     
-    prodDm=aggregate(prodD/1000, by=as.yearmon, mean, na.rm=1)
-    prody=aggregate(prodD/1000, by=year, sum, na.rm=1)
+    #prodDm=aggregate(prodD/1000, by=as.yearmon, mean, na.rm=1)
+    #prody=aggregate(prodD/1000, by=year, sum, na.rm=1)
 
-    prodDm$Yf=prodDm$Yf*1000
-    prody$Yf=prody$Yf*1000
-  }
-  
+    #prodDm$Yf=prodDm$Yf*1000
+    #prody$Yf=prody$Yf*1000
+  #}
+
+    d <- truncDay(prodI$Dates)
+    d <- unique(d)
+    by <- radEf@sample
+    
+    prodDm <- prodI[, .(Eac = P2E(Pac, by)/1000,
+                        Edc = P2E(Pdc, by)/1000),
+                    by = .(month(truncDay(Dates)), year(truncDay(Dates)))]
+    prodDm[, Yf := Eac/(Pg/1000)]
+    
+    if(radEf@type == 'prom'){
+        prodD <- prodDm[, .(Eac = Eac*1000,
+                            Edc = Edc*1000,
+                            Yf),
+                        by = d]
+        
+        prody <- prodDm[, .(Eac = sum(Eac*DayOfMonth, na.rm = TRUE),
+                            Edc = sum(Edc*DayOfMonth, na.rm = TRUE),
+                            Yf = sum(Yf*DayOfMonth, na.rm = TRUE))]
+    } else {
+        GefD <- prodI[, .(Eac = P2E(Pac, by)/1000,
+                          Edc = P2E(Pdc, by)/1000,
+                          Yf = Eac/Pg),
+                      by = truncDay(Dates)]
+        Gefy <- GefD[, .(Eac = sum(Eac, na.rm = TRUE)/1000,
+                         Edc = sum(Edc, na.rm = TRUE)/1000,
+                         Yf = sum(Yf, na.rm = TRUE)),
+                     by = year(d)]
+    }
+
+    prodDm[, Dates := paste(month.abb[month], 'of', year)]
+    prodDm[, c('month', 'year') := NULL]
+    setcolorder(prodDm, c('Dates', names(prodDm)[-length(prodDm)]))
+    names(prody)[1] <- 'Dates'
+    names(prodD)[1] <- 'Dates'
+    
   result <- new('ProdGCPV',
                 radEf,                  #contains 'Gef'
                 prodD=prodD,
