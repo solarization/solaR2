a7d64f310ce2fbcbeac026e06e1a619d7a864430
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-07-11 13:22:48 +0200

    Update fSombra6.R

--- R/fSombra6.R
+++ R/fSombra6.R
@@ -4,16 +4,19 @@ fSombra6<-function(angGen, distances, struct, prom=TRUE)
               is.data.frame(distances))
     ##distances sólo tiene tres distances, así que genero una cuadrícula
     if (dim(distances)[1]==1){ 
-        Red<-with(distances,
-                  data.frame(Lew=c(-Lew,0,Lew,-Lew,Lew),
-                             Lns=c(Lns,Lns,Lns,0,0),
-                             H=H))
+        ##Red<-with(distances,
+                  ##data.frame(Lew=c(-Lew,0,Lew,-Lew,Lew),
+                             ##Lns=c(Lns,Lns,Lns,0,0),
+                             ##H=H))
+        Red <- distances[, .(Lew = c(-Lew, 0, Lew, -Lew, Lew),
+                             Lns = c(Lns, Lns, Lns, 0, 0),
+                             H=H)]
     } else { #distances es una matriz, luego no hace falta generar la cuadrícula
         Red<-distances[1:5,]} #Sólo necesito las 5 primeras filas...necesario por sí  se entrega un data.frame erroneo
 
     ## Calculo la sombra debida a cada uno de los 5 seguidores
-    SombraGrupo<-matrix(ncol=5,nrow=dim(angGen)[1])
-    for (i in 1:5) {SombraGrupo[,i]<-coredata(fSombra2X(angGen,Red[i,],struct))}
+    SombraGrupo<-matrix(ncol=5,nrow=dim(angGen)[1]) ### VECTORIZAR
+    for (i in 1:5) {SombraGrupo[,i]<-fSombra2X(angGen,Red[i,],struct)}
     ##Para calcular la Sombra Promedio, necesito el número de seguidores en cada posición(distrib)
     distrib=with(struct,c(1,Ncol-2,1,Nrow-1,(Ncol-2)*(Nrow-1),Nrow-1)) 
     vProm=c(sum(distrib[c(5,6)]),
@@ -32,5 +35,5 @@ fSombra6<-function(angGen, distances, struct, prom=TRUE)
         ## Factor de sombra en el seguidor #5 debido a los otros 5 seguidores
         FS=rowSums(SombraGrupo)
         FS[FS>1]<-1}
-    return(zoo(FS, index(angGen)))
+    return(FS)
 }
