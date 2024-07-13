a14fcc3192311fe1224c4f73406455063c91d163
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-07-13 14:23:06 +0200

    Update compare.R

--- R/compare.R
+++ R/compare.R
@@ -8,26 +8,27 @@ if(getRversion() >= "2.15.1") globalVariables(c('name', 'x', 'y', 'group.value')
 setGeneric('compare', signature='...', function(...){standardGeneric('compare')})
 
 compareFunction <- function(..., vars){
-  dots <- list(...)
-  nms0 <- substitute(list(...))
-  if (!is.null(names(nms0))){ ##estamos dentro de do.call
-    nms <- names(nms0[-1])
-  } else {
-    nms <- as.character(nms0[-1])
-  }
-  foo <- function(object, label){
-    yY <- colMeans(as.data.frameY(object, complete=TRUE)[vars])
-    yY <- cbind(stack(yY), name=label)
-    yY
-  }
-  cdata <- mapply(FUN=foo, dots, nms, SIMPLIFY=FALSE)
-  z <- do.call(rbind, cdata)
-  z$ind <- ordered(z$ind, levels=vars)
-  p <- dotplot(ind~values, groups=name, data=z, type='b',
-               par.settings=solaR.theme)
-  print(p+glayer(panel.text(x[length(x)], y[length(x)],
-                            label=group.value, cex=0.7, pos=3, srt=45)))
-  return(z)
+    dots <- list(...)
+    nms0 <- substitute(list(...))
+    if (!is.null(names(nms0))){ ##estamos dentro de do.call
+        nms <- names(nms0[-1])
+    } else {
+        nms <- as.character(nms0[-1])
+    }
+    foo <- function(object, label){
+        ###yY <- colMeans(as.data.frameY(object, complete=TRUE)[vars])
+        yY <- colMeans(as.data.tableY(object, complete = TRUE)[, ..vars])
+        yY <- cbind(stack(yY), name=label)
+        yY
+    }
+    cdata <- mapply(FUN=foo, dots, nms, SIMPLIFY=FALSE)
+    z <- do.call(rbind, cdata)
+    z$ind <- ordered(z$ind, levels=vars)
+    p <- dotplot(ind~values, groups=name, data=z, type='b',
+                 par.settings=solaR.theme)
+    print(p+glayer(panel.text(x[length(x)], y[length(x)],
+                              label=group.value, cex=0.7, pos=3, srt=45)))
+    return(z)
 }
 
 
