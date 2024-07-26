f5f94be3f53ce00407ba1cc8e5d826fb0c261166
Francisco Delgado López <47789266+fdelgadol@users.noreply.github.com>
2024-07-26 20:24:47 +0200

    Update writeSolar.R

--- R/writeSolar.R
+++ R/writeSolar.R
@@ -6,33 +6,31 @@ setGeneric('writeSolar', function(object, file,
 
 setMethod('writeSolar', signature=(object='Sol'),
           definition=function(object, file, complete=FALSE, day=FALSE,
-          timeScales=c('i', 'd', 'm', 'y'), sep=',', ...){
+                              timeScales=c('i', 'd', 'm', 'y'), sep=',', ...){
               name <- strsplit(file, '\\.')[[1]][1]
               ext <- strsplit(file, '\\.')[[1]][2]
               timeScales <- match.arg(timeScales, several.ok=TRUE)
               if ('i' %in% timeScales) {
-                  zI <- as.zooI(object, complete=complete, day=day)
-                  write.zoo(zI, file=file, sep=sep, ...)
+                  zI <- as.data.tableI(object, complete=complete, day=day)
+                  write.table(zI,
+                              file=file, sep=sep, row.names = FALSE, ...)
               }
               if ('d' %in% timeScales) {
-                  zD <- as.zooD(object, complete=complete)
-                  write.zoo(zD,
+                  zD <- as.data.tableD(object, complete=complete, day = day)
+                  write.table(zD,
                             file=paste(name, 'D', ext, sep='.'),
-                            sep=sep, ...)
+                            sep=sep, row.names = FALSE, ...)
               }
-
               if ('m' %in% timeScales) {
-                  zM <- as.zooM(object, complete=complete)
-                  write.zoo(zM,
+                  zM <- as.data.tableM(object, complete=complete, day = day)
+                  write.table(zM,
                             file=paste(name, 'M', ext, sep='.'),
-                            sep=sep, ...)
+                            sep=sep, row.names = FALSE, ...)
               }
-
               if ('y' %in% timeScales) {
-                  zY <- as.zooY(object, complete=complete)
-                  write.zoo(zY,
+                  zY <- as.data.tableY(object, complete=complete, day = day)
+                  write.table(zY,
                             file=paste(name, 'Y', ext, sep='.'),
-                            sep=sep, ...)
+                            sep=sep, row.names = FALSE, ...)
               }
-
           })
