41cc8995f4295eaf965ae2a620f59226930e6b59
Francisco Delgado López <f.delgadol@alumnos.upm.es>
2024-07-28 14:05:23 +0200

    updated analyzeData and TargetDiagram

--- R/TargetDiagram.R
+++ R/TargetDiagram.R
@@ -1,5 +1,4 @@
 TargetDiagram<-function(x, end, ndays, ref=NULL, color=NULL, cex=0.8, ...){
-    .Defunct(new = "tdr::targetDiagram", package = "solaR")
-    
+    .Defunct(new = "tdr::targetDiagram", package = "solaR2")    
 }
 
